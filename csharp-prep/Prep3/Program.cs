8880f2eb257f9b51ec897690beda26bc2a38b6a9
Joan Robert Picazo <Joanrpicazo@gmail.com>
2026-01-15 07:29:52 -0300

    Completed Prep 3 Magic Number Guess Game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain;
+        
+        // Create a loop for the user to keep playing
+        do
+        {
+            // Generate the magic number randomly
+            Random randomGenerator = new Random();
+            int magicNum = randomGenerator.Next(1, 101);
+            
+            int counter = 0;
+            double guess;
+            
+            do
+            {
+                // Ask user for their Guess
+                Console.Write("What is your guess?: ");
+                guess = double.Parse(Console.ReadLine());
+            
+                // increase counter by 1
+                counter++; 
+            
+                // Tell the user they need to guess higher
+                if (magicNum > guess)
+                {
+                    Console.WriteLine("Guess Higher");
+                }
+                // Tell the user they need to guess lower
+                else if (magicNum < guess)
+                {
+                    Console.WriteLine("Guess Lower");
+                }
+            } while (magicNum != guess); 
+        
+            // Tell the user they have guessed the magic number
+            Console.WriteLine($"You guessed it the magic number was: {magicNum}!");
+            Console.WriteLine($"It took you {counter} to guess the magic number!");
+        
+            // Ask user if they would like to play again
+            Console.Write("Would you like to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+        } while (playAgain == "yes");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Hello World!");
+        Console.WriteLine("Hello World!");
     }
 }
